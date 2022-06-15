f2e40afd39d01849108b9071bafff4ed4f7b79c7
Yuta0227 <yutahonjo@keio.jp>
2022-06-15 18:38:53 +0900

    クイズからクイズ一覧に戻れるようにした

--- src/resources/views/quiz/quiz.blade.php
+++ src/resources/views/quiz/quiz.blade.php
@@ -9,26 +9,40 @@
     <link rel="stylesheet" href="{{ asset('css/quiz.css') }}">
 </head>
 <a href="{{ route('quiz.quiz_list') }}">一覧に戻る</a>
+
 <body>
     <h1>{{ $quiz_titles[0]->big_question_title }}</h1>
     <div class="main">
         @foreach ($question_list as $question)
-            <div class="quiz">
+            <div class="quiz" id="question{{ $loop->iteration }}">
                 <h1>{{ $loop->iteration }}. この地名はなんて読む？</h1>
-                <img alt="{{ $correct_answers[$loop->iteration-1]->choice_name }}の画像" src="{{ asset("img/".$pictures[$loop->iteration-1]->picture_url) }}">
+                <img alt="{{ $correct_answers[$loop->iteration - 1]->choice_name }}の画像"
+                    src="{{ asset('img/' . $pictures[$loop->iteration - 1]->picture_url) }}">
                 <ul>
-                    <li id="answerlist_{{ $loop->iteration }}_1" name="answerlist_{{ $loop->iteration }}"
-                        class="answerlist" onclick="check({{ $loop->iteration }},1, '{{ $correct_answers[$loop->iteration-1]->choice_name }}')">{{ $question[0]->choice_name }}
-                    </li>
-                    <li id="answerlist_{{ $loop->iteration }}_2" name="answerlist_{{ $loop->iteration }}"
-                        class="answerlist" onclick="check({{ $loop->iteration }},2, '{{ $correct_answers[$loop->iteration-1]->choice_name }}')">{{ $question[1]->choice_name }}
-                    </li>
-                    <li id="answerlist_{{ $loop->iteration }}_3" name="answerlist_{{ $loop->iteration }}"
-                        class="answerlist" onclick="check({{ $loop->iteration }},3, '{{ $correct_answers[$loop->iteration-1]->choice_name }}')">{{ $question[2]->choice_name }}
-                    </li>
+                    <a href="#question{{ $loop->iteration + 1 }}" class="link_style_none">
+                        <li id="answerlist_{{ $loop->iteration }}_1" name="answerlist_{{ $loop->iteration }}"
+                            class="answerlist"
+                            onclick="check({{ $loop->iteration }},1, '{{ $correct_answers[$loop->iteration - 1]->choice_name }}')">
+                            {{ $question[0]->choice_name }}
+                        </li>
+                    </a>
+                    <a href="#question{{ $loop->iteration + 1 }}" class="link_style_none">
+                        <li id="answerlist_{{ $loop->iteration }}_2" name="answerlist_{{ $loop->iteration }}"
+                            class="answerlist"
+                            onclick="check({{ $loop->iteration }},2, '{{ $correct_answers[$loop->iteration - 1]->choice_name }}')">
+                            {{ $question[1]->choice_name }}
+                        </li>
+                    </a>
+                    <a href="#question{{ $loop->iteration + 1 }}" class="link_style_none">
+                        <li id="answerlist_{{ $loop->iteration }}_3" name="answerlist_{{ $loop->iteration }}"
+                            class="answerlist"
+                            onclick="check({{ $loop->iteration }},3, '{{ $correct_answers[$loop->iteration - 1]->choice_name }}')">
+                            {{ $question[2]->choice_name }}
+                        </li>
+                    </a>
                     <li id="answerbox_{{ $loop->iteration }}" class="answerbox">
                         <span id="answertext_{{ $loop->iteration }}"></span><br>
-                        <span>正解は「{{ $correct_answers[$loop->iteration-1]->choice_name }}」です！</span>
+                        <span>正解は「{{ $correct_answers[$loop->iteration - 1]->choice_name }}」です！</span>
                     </li>
                 </ul>
             </div>
--- src/resources/views/quiz/quiz_list.blade.php
+++ src/resources/views/quiz/quiz_list.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ route('quiz.quiz',['big_question_index'=>1]) }}">東京</a>
-<a href="{{ route('quiz.quiz',['big_question_index'=>2]) }}">広島</a>
+<a href="{{ route('quiz.quiz',['big_question_id'=>1]) }}">東京</a>
+<a href="{{ route('quiz.quiz',['big_question_id'=>2]) }}">広島</a>
