07fa69023f703f6abc71c764615fd0f46b25f09a
Yuta0227 <yutahonjo@keio.jp>
2022-06-22 21:18:39 +0900

    クイズの長さもeagerloadできた

--- src/app/Http/Controllers/QuestionListController.php
+++ src/app/Http/Controllers/QuestionListController.php
@@ -8,32 +8,40 @@ use App\QuestionList;
 
 class QuestionListController extends Controller
 {
+    public function load_choices($big_question_id)
+    {
+        return BigQuestion::with('choices')->find($big_question_id);
+    }
     public function questions($big_question_id)
     {
         $question_lists = [];
         $quiz_length = $this->quiz_length($big_question_id);
-        $choices=BigQuestion::with('choices')->find($big_question_id);
-        for($question_id=1;$question_id<=$quiz_length;$question_id++){
-            $before_shuffle=[];
-            $before_shuffle=$choices->choices->filter(function($choice) use ($question_id) {
-                return $choice->question_id==$question_id;
+        $choices = $this->load_choices($big_question_id);
+        for ($question_id = 1; $question_id <= $quiz_length; $question_id++) {
+            $before_shuffle = [];
+            $before_shuffle = $choices->choices->filter(function ($choice) use ($question_id) {
+                return $choice->question_id == $question_id;
             });
-            $after_shuffle=$before_shuffle->shuffle();            
-            array_push($question_lists,$after_shuffle);
+            $after_shuffle = $before_shuffle->shuffle();
+            array_push($question_lists, $after_shuffle);
         }
         return $question_lists;
     }
     function quiz_length($big_question_id)
     {
-        return BigQuestion::find($big_question_id)->choices()->getQuizLength();
+        return count($this->load_choices($big_question_id)->choices->unique('question_id'));
     }
     public function correct_answer($big_question_id)
     {
-        $quiz_length = $this->quiz_length($big_question_id);
         $correct_answer_array = [];
-        for ($question_id = 1; $question_id <= $quiz_length; $question_id++) {
-            $correct_answer = BigQuestion::find($big_question_id)->choices()->questionIdEqual($question_id)->isCorrect()->first();
-            array_push($correct_answer_array, $correct_answer);
+        $correct_answer = $this->load_choices($big_question_id)->choices
+            ->filter(function ($choice) {
+                return $choice->valid == true;
+            })->all();
+        foreach ($correct_answer as $answer) {
+            //$correct_answerだとキーが設定されてるため取得が無理。pushしなおすとキーがなくなる
+            //こういうコードの意図を説明するようなコメントは残すべき？
+            array_push($correct_answer_array, $answer);
         }
         return $correct_answer_array;
     }
